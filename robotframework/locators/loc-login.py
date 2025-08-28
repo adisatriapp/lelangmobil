92601f2e0e60200f0bded891377e7f1828212e02
adisatriapp <adisatriapp@gmail.com>
2025-08-29 06:19:50 +0700

    change xpath daftar

--- robotframework/locators/loc-login.py
+++ robotframework/locators/loc-login.py
@@ -4,5 +4,5 @@ btn_login = "//button[normalize-space()='Masuk']"
 checkbox_syarat = "//input[@id='remember_me']"
 frame_recaptcha = "//iframe[contains(@title, 'reCAPTCHA')]"
 checkbox_captcha = "//div[@class='recaptcha-checkbox-border']"
-btn_daftar = "//a[text()='Daftar Sekarang']"
+btn_daftar = "//a[normalize-space()='Daftar Sekarang']"
 
